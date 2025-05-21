b8a1260673a73286d9aa81419b86a19c53a8ef8f
_SparkHere <116731471+sparkhere-sys@users.noreply.github.com>
2025-05-21 17:35:40 +0300

    v1.1.0 - script updated

--- script/archapt.sh
+++ script/archapt.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
 
-# archapt v1.0.2
-# github: https://github.com/sparkhere-sys/archapt
+# archapt v1.1.0 - made with <3 by Spark
 
+# default variables
 skip=0 # 0 = false, 1 = true
+usesyu=1 # 2 = use -Sy
+flag_noupgrade=0
+flag_update=0
+args=()
+flags=()
+
+# uncomment the lines below if you want to use the old system for parsing flags
+# for arg in "$@"; do
+#  if [[ $arg == "--noconfirm" ]]; then
+#    skip=1
+#    set -- "${@//$arg/}"
+#    break
+#  elif [[ $arg == "--noupgrade" ]]; then
+#    usesyu=0
+#    set -- "${@//$arg/}"
+#    break
+#  fi
+# done
+
+# FLAG PARSING
 for arg in "$@"; do
-  if [[ $arg == "--noconfirm" ]]; then
-    skip=1
-    set -- "${@//$arg/}"
-    break
-  fi
+  case "$arg" in
+    --noconfirm)
+      skip=1
+      ;;
+    --noupgrade)
+      usesyu=0
+      flag_upgrade=1
+      ;;
+    --update)
+      usesyu=2
+      flag_update=1
+      ;;
+    -*)
+      flags+=("$arg")
+      ;;
+    *)
+      args+=("$arg")
+      ;;
+  esac
 done
+
+if [[ $flag_upgrade -eq 1 && $flag_update -eq 1 ]]; then
+  echo "Error: You cannot use --noupgrade and --update at the same time."
+  exit 1
+fi
+
+help() {
+  echo "Supported Commands:"
+  echo "install"
+  echo "remove, autoremove"
+  echo "update"
+  echo "upgrade"
+  echo "search"
+  echo "list"
+  echo "info"
+  echo "...but not moo."
+  echo "Flags:"
+  echo "--noconfirm: Skips archapt and pacman's confirm prompts."
+  echo "--noupgrade: Uses -S instead of -Syu. Does not work with --update."
+  echo "--update: Uses -Sy instead of -Syu. Does not work with --noupgrade."
+  echo "Usage: apt <command> <package> <flags>"
+  echo "archapt - Made with <3 by Spark"
+}
 
 run() {
   echo "+ Running: $*"
@@ -27,14 +84,20 @@ run() {
 }
 
 translate() {
-  case "$1" in
+  cmd="${args[0]}"
+  packages=("${args[@]:1}")
+  
+  case "$cmd" in
     install)
-      shift
-      run "sudo pacman -Syu ${skip:+--noconfirm} $*"
+      if [[ $usesyu -eq 1 ]]; then
+        run "sudo pacman -Syu ${skip:+--noconfirm} ${packages[*]}"
+      elif [[ $usesyu -eq 2 ]]; then
+        run "sudo pacman -Sy ${skip:+--noconfirm} ${packages[*]}"
+      else
+        run "sudo pacman -S ${skip:+--noconfirm} ${packages[*]}"
       ;;
-    remove)
-      shift
-      run "sudo pacman -Rns ${skip:+--noconfirm} $*"
+    remove|autoremove)
+      run "sudo pacman -Rns ${skip:+--noconfirm} ${packages[*]}"
       ;;
     update)
       run "sudo pacman -Sy ${skip:+--noconfirm}"
@@ -43,29 +106,26 @@ translate() {
       run "sudo pacman -Syu ${skip:+--noconfirm}"
       ;;
     search)
-      shift
-      run "sudo pacman -Ss $*"
+      run "sudo pacman -Ss ${packages[*]}"
       ;;
     list)
       run "sudo pacman -Q" 
       ;;
     info)
-      shift
-      run "sudo pacman -Qi $*"
+      run "sudo pacman -Qi ${packages[*]}"
       ;;
     moo)
-      echo "brother this is not debian"
+      echo "brother this is not debian" # i am not adding the ascii cow
       ;;
     *)
-      echo "Invalid command. Available commands: install, remove, update, upgrade, search, list, info"
+      echo "Invalid command: $cmd"
+      help
       ;;
   esac
 }
 
 if [[ $# -eq 0 ]]; then
-  echo "Usage: apt <command> <pkg> <flags>"
-  echo "Available commands: install, remove, update, upgrade, search, list, info"
-  echo "Available flags: --noconfirm"
+  help
   exit 1
 fi
 
