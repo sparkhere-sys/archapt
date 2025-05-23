684bee3322ca5731c08a3e99c2bbca67f7e5cc14
_SparkHere <116731471+sparkhere-sys@users.noreply.github.com>
2025-05-23 06:46:15 +0300

    renamed v1.1.1 to v1.1.0, changed some stuff, look in the blame

--- script/archapt.sh
+++ script/archapt.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# archapt v1.1.1 - made with <3 by Spark
+# archapt v1.1.0 - made with <3 by Spark
 
 # default variables
 skip=0 # 0 = false, 1 = true
 usesyu=1 # 2 = use -Sy
 norun=0
+norns=0
 flag_upgrade=0
 flag_update=0
 args=()
@@ -28,6 +29,9 @@ for arg in "$@"; do
     --dry-run)
       norun=1
       ;;
+    --dont-remove-depends)
+      norns=1
+      ;;
     -*)
       flags+=("$arg")
       ;;
@@ -39,7 +43,20 @@ done
 
 if [[ $flag_upgrade -eq 1 && $flag_update -eq 1 ]]; then
   echo "Error: You cannot use --noupgrade and --update at the same time."
-  exit 1
+  echo "(1) --noupgrade"
+  echo "(2) --update"
+  echo "(q) exit"
+  echo "Pick an option from the above."
+  read -p "> " confirm2
+  if [[ "$confirm2" -eq 1 ]]; then
+      flag_update=0
+      usesyu=0
+  elif [[ "$confirm2" -eq 2 ]]; then
+      flag_upgrade=0
+      usesyu=2
+  else
+    exit 1
+  fi
 fi
 
 help() {
@@ -56,6 +73,7 @@ help() {
   echo "--noconfirm: Skips archapt and pacman's confirm prompts."
   echo "--noupgrade: Uses -S instead of -Syu. Does not work with --update."
   echo "--update: Uses -Sy instead of -Syu. Does not work with --noupgrade."
+  echo "--dry-run: Doesn't actually run the pacman command. It's only there for your self-esteem."
   echo "Usage: apt <command> <package> <flags>"
   echo "archapt - Made with <3 by Spark"
 }
@@ -64,12 +82,14 @@ run() {
   echo "+ Running: $*"
   if [[ $norun -eq 1 ]]; then
     echo "(dry run)"
+    exit 0
   fi
   if [[ $skip -eq 1 ]]; then
     bash -c "$*"
     return
   fi
-  read -p "Proceed? [Y/n]: " confirm
+  echo "Proceed? [Y/n]"
+  read -p "> " confirm
   if [[ "$confirm" =~ ^[Nn]$ ]]; then
     echo "Aborted."
   else
@@ -91,8 +111,12 @@ translate() {
         run "sudo pacman -S ${skip:+--noconfirm} ${packages[*]}"
       fi
       ;;
-    remove|autoremove)
-      run "sudo pacman -Rns ${skip:+--noconfirm} ${packages[*]}"
+    remove|autoremove|uninstall)
+      if [[ $norns -eq 0 ]]; then
+        run "sudo pacman -Rns ${skip:+--noconfirm} ${packages[*]}"
+      else
+        run "sudo pacman -R ${skip:+--noconfirm} ${packages[*]}"
+      fi
       ;;
     update)
       run "sudo pacman -Sy ${skip:+--noconfirm}"
@@ -111,6 +135,9 @@ translate() {
       ;;
     moo)
       echo "brother this is not debian" # i am not adding the ascii cow
+      ;;
+    help|-h)
+      help
       ;;
     *)
       echo "Invalid command: $cmd"
